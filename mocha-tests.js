8b1ead2bc4eee9e013127e822e03eb4b32cb996d
Артем <WWW_cool@inbox.ru>
2022-12-15 16:05:43 +0400

    fixed local to run

--- mocha-tests.js
+++ mocha-tests.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const settings = require('./build/settings');
+const glob = require('glob');
 
 const { mocha, args } = settings.setup(process.argv);
 
 if (args['file']) {
     mocha.addFile(path.resolve(args['file']));
 } else {
-    glob.sync('**/*.js', { cwd: testDir })
-        .forEach(file => mocha.addFile(path.resolve(testDir, file)));
+    glob.sync('**/*.js', { cwd: "./build/test/" })
+        .forEach(file => mocha.addFile(path.resolve("./build/test/", file)));
 }
 
 mocha
